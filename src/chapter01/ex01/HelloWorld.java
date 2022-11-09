950ca66e558435cbb4778420f97922b3ad45208c
Juseong Park <juseong001214@gmail.com>
2022-11-09 10:40:49 +0900

    첫번째 커밋

---
+++ src/chapter01/ex01/HelloWorld.java
@@ -0,0 +1,24 @@
+package chapter01.ex01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 주석 : 한 라인 주석 입니다.
+			// 주석은 프로그램에서 실행되지 않는 부분입니다.
+		
+		/*
+		 * 여러 라인 주석은 /* 스페이스바
+		 */
+		
+		System.out.println("출력 입니다.");
+		
+		System.out.println("출력 2 입니다.");
+		
+		System.out.println("출력 3 입니다.");
+		//syso = <Ctrl+SpaceBar> 자동완성
+		
+		
+	}
+
+}
---
+++ src/prac01/ex02/Hi.java
@@ -0,0 +1,10 @@
+package prac01.ex02;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
